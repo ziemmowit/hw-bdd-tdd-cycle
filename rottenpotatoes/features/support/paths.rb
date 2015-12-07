5dfdde8a0301c04a0674765281cdc4811a619e5b
Ziemmowit R <ziemowit@zwr.co>
2015-12-07 17:15:17 +0000

    Scenarios + first scenario finished

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -21,7 +21,8 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    when /^the edit page for "(.*)"$/
+      edit_movie_path(Movie.find_by_title($1))
     else
       begin
         page_name =~ /^the (.*) page$/
